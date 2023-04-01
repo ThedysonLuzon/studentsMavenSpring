8b59205ae2122b7cd3e8c231c57e2ebd3030a249
Thedyson Luzon <luzon.thedyson@gmail.com@users.noreply.github.com>
2023-04-01 08:51:56 -0400

    CRUD working

--- src/main/java/com/spring/jpa/controller/StudentController.java
+++ src/main/java/com/spring/jpa/controller/StudentController.java
@@ -1,10 +1,13 @@
 package com.spring.jpa.controller;
 
+import com.spring.jpa.model.Student;
 import com.spring.jpa.repository.StudentRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 
@@ -18,5 +21,46 @@ public class StudentController {
         model.addAttribute("students", studentRepository.findAll());
         return "index";
     }
+
+    @GetMapping("/add")
+    public String getAddStudentForm(Student student){
+        return "add-student";
+    }
+    @PostMapping("/add")
+    public String insertStudentData(@Valid Student student, BindingResult bindingResult, Model model){
+
+        studentRepository.save(student);
+        model.addAttribute("students", studentRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteStudent(@PathVariable("id")int id, Model model){
+        Student student = studentRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("Invalid Student ID" + id));
+        studentRepository.delete(student);
+        model.addAttribute("students",studentRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/update/{id}")
+    public String moveToUpdateEmployeePage(@PathVariable("id")int id, Model model){
+        Student student = studentRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("Invalid Employee ID" + id));
+        model.addAttribute("student",student);
+        return "update-student";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateEmployee(@PathVariable("id")int id,@Valid Student student,BindingResult bindingResult,Model model){
+        if(bindingResult.hasErrors()){
+            student.setId(id);
+            return "update-employee";
+        }
+        studentRepository.save(student);
+        model.addAttribute("students",studentRepository.findAll());
+        return "index";
+    }
+
 
 }
